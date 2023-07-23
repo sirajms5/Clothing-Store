703a6f5b5821cbce585fafe29c57dab903bf30ea
Siraj Mohammed Saleem <sirajms5@yahoo.com>
2023-07-23 13:27:18 -0400

    updated query

--- php/payment.php
+++ php/payment.php
@@ -8,7 +8,7 @@
     $resultPayment = mysqli_query($conn, $queryPayment);
 
     if($resultPayment){
-        $querryUpdateUserCart = "UPDATE carts SET End_Date = NOW() WHERE User_Id = $userId AND End_Date IS NULL;";
+        $querryUpdateUserCart = "UPDATE carts SET End_Date = NOW(), Transaction_Id = (SELECT MAX(id) FROM transactions) WHERE User_Id = $userId AND End_Date IS NULL;";
         $resultUpdateUserCart = mysqli_query($conn, $querryUpdateUserCart);
         if($resultUpdateUserCart){
             echo "paid";
